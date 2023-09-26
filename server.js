a9f3343528bad1ed444e8acf8b89aab748c1ee42
zaykhere <zainjhere@gmail.com>
2023-09-26 20:07:41 +0500

    Adding prisma and express server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+prisma.$connect()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+      console.log("Connected to the database.");
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+
+process.on('SIGINT', async () => {
+  console.log('Received SIGINT. Closing Prisma connection...');
+
+  await prisma.$disconnect(); // Close the Prisma connection
+
+  console.log('Prisma connection closed. Exiting...');
+  process.exit(0);
+});
