d341774f6fd9023a7761ed237cd9b316b0aa93b8
zaykhere <zainjhere@gmail.com>
2023-09-26 21:53:10 +0500

    login with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require("bcryptjs");
+const generateToken = require('../utils/generateToken');
 
 async function loginUser(req, res) {
   const {email, password} = req.body;
@@ -20,7 +21,10 @@ async function loginUser(req, res) {
       return res.status(400).json({error: "Invalid Credentials"});
     }
 
-    res.status(200).json(user);
+    res.status(200).json({
+      user,
+      token: generateToken(user.id)
+    });
 
   } catch (error) {
     console.log(error);
